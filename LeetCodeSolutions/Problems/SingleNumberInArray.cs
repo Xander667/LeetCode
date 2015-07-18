0539a021f2084905eb7cb9082a9bb58ab8fa3015
Xander667 <xanderh@hotmail.co.uk>
2015-07-17 17:53:43 -0700

    XOR interview question. 5 xor 5 = 0. God for finding singles

---
+++ LeetCodeSolutions/Problems/SingleNumberInArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Problems
+{
+    class SingleNumberInArray
+    {
+        
+        public static int SingleNumber(int[] nums)
+        {
+            int result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result = result ^ nums[i];
+            }
+            return result;
+        }
+
+    }
+}
--- LeetCodeSolutions/Program.cs
+++ LeetCodeSolutions/Program.cs
@@ -11,6 +11,7 @@ namespace LeetCodeSolutions
     {
         static void Main(string[] args)
         {
+            int num = SingleNumberInArray.SingleNumber(new int[] { 5, 5, 6, 6, 7 });
             bool result = Palindrome.TestPalindromes(); //true = pass, false = fail
         }
     }
